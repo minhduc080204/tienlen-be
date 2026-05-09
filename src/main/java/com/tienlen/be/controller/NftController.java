3d66596b488f17cf854215c8066d4dd5ddae6319
minh Duc <minhduc08022004@gmail.com>
2026-05-09 21:22:35 +0700

    fix: update NftController API endpoint versioning

--- src/main/java/com/tienlen/be/controller/NftController.java
+++ src/main/java/com/tienlen/be/controller/NftController.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/nfts")
+@RequestMapping("/api/nfts")
 @RequiredArgsConstructor
 public class NftController {
 
